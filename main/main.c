4039531b9ece558d779f00b4b00b8d05e0c28142
IDTenev <morlion7@gmail.com>
2026-02-23 09:44:08 +0900

    main 정리

--- main/main.c
+++ main/main.c
@@ -20,31 +20,6 @@
 #include "d_ws2812.h"
 #include "s_udp.h"
 
-static void udp_echo_task(void *arg)
-{
-    s_udp_handle_t udp;
-    s_udp_open(&udp, 0, 5000);
-
-    ESP_LOGI("APP/UDP", "UDP echo started via service");
-
-    while (1) {
-        s_udp_peer_t peer;
-        uint8_t buf[512];
-
-        int32_t n = s_udp_recvfrom(&udp, &peer, buf, sizeof(buf));
-        if (n > 0) {
-            ESP_LOGI("APP/UDP", "RX from %u.%u.%u.%u:%u len=%d",
-                     peer.ip[0], peer.ip[1], peer.ip[2], peer.ip[3],
-                     (unsigned)peer.port, (int)n);
-
-            s_udp_sendto(&udp, &peer, buf, (size_t)n);
-            ESP_LOGI("APP/UDP", "TX echo len=%d", (int)n);
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
-}
-
 void app_main(void)
 {
     printf("Boot Water v1.1\n");
@@ -63,8 +38,6 @@ void app_main(void)
     ws2812_init();
 
     printf("Start Water-Link v1.0\n");
-    xTaskCreate(udp_echo_task, "udp_echo", 4096, NULL, 3, NULL);
-
 
     while (1) {
 
