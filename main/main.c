1ccf8afbc002932e498be88a7f7fbc4e46a82bcd
IDTenev <morlion7@gmail.com>
2026-02-25 19:31:06 +0900

    main정리

--- main/main.c
+++ main/main.c
@@ -20,30 +20,7 @@
 #include "d_ws2812.h"
 #include "d_ht16k33.h"
 
-#include "s_udp.h"
-#include "s_dhcp.h"
-#include "s_ping.h"
-
 #define TAG "APP/MAIN"
-
-static void ping_task(void *arg)
-{
-    const uint8_t target[4] = {192,168,10,1};   // 라우터/NAT 환경에서만 의미 있음
-    // PC 직결이면 192.168.10.1 같은 걸로
-
-    while (1) {
-        s_ping_result_t r = { .id = 0x1234, .seq = 1 };
-
-        bool ok = s_ping_once(2, target, 1000, &r);
-        if (ok) {
-            ESP_LOGI("APP/PING", "PING OK rtt=%ums", (unsigned)r.rtt_ms);
-        } else {
-            ESP_LOGW("APP/PING", "PING TIMEOUT");
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
 
 void app_main(void)
 {
@@ -66,8 +43,6 @@ void app_main(void)
     w5500_init();
     ws2812_init();
     //ht16k33_init(HT16K33_ADDRESS);
-    
-    xTaskCreate(ping_task, "ping", 4096, NULL, 3, NULL);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(1000));
